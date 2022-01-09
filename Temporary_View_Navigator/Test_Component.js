62add9d95495da6941a86cc71698400b4a3d3fc5
SamPettus <spettus@umail.ucsb.edu>
2022-01-08 23:53:52 -0800

    added navigation portal

---
+++ Temporary_View_Navigator/Test_Component.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
+const Temporary_View_Navigator = (props = {navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text>Temporary Navigator</Text>
+      <StatusBar style="auto" />
+      <Button 
+        onPress={() => props.navigation.navigate("Test_Component 2")}
+        title="Navigate to Grid"
+        >
+        Navigate
+      </Button>
+    </View>
+  );
+};
+
+const Test_Component2 = () => {
+  return (
+    <View style={styles.container}>
+      <Text>View 2!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export { Temporary_View_Navigator, Test_Component2 };
