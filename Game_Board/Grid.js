de4d13617935125e00453cfb773c14ea1fe8852f
Joseph Nahm <josephnahm1@gmail.com>
2022-01-09 19:48:16 -0800

    lines rendered properly

--- Game_Board/Grid.js
+++ Game_Board/Grid.js
@@ -2,14 +2,35 @@ import { StyleSheet, Text, View } from 'react-native';
 import sudoku from './gameLogic';
 import { useState } from 'react';
 
-var Cell = ({cell}) => {
-  return <View style={styles.cell}><Text>{cell}</Text></View>
+var Cell = ({cell, margin}) => {
+  var margins = margin ? margin.map(dir => styles[dir]) : [];
+  return <View style={[styles.cell, ...margins]}><Text>{cell}</Text></View>
 }
 
 var Square = ({square, margin}) => {
   var margins = margin ? margin.map(dir => styles[dir]) : [];
   return <View style={[styles.square, ...margins]}>
-    {square.map((cell, i) => <Cell key={i} cell={cell}/>)}
+    <Cell cell={square[0]} margin={['cellTop', 'left']}/>
+    <View style={styles.sideLine}></View>
+    <Cell cell={square[1]}/>
+    <View style={styles.sideLine}></View>
+    <Cell cell={square[2]}/>
+    <View style={styles.bottomLine}></View>
+    <View style={styles.bottomLine}></View>
+    <View style={styles.bottomLine}></View>
+    <Cell cell={square[3]} margin={['left']}/>
+    <View style={styles.sideLine}></View>
+    <Cell cell={square[4]}/>
+    <View style={styles.sideLine}></View>
+    <Cell cell={square[5]}/>
+    <View style={styles.bottomLine}></View>
+    <View style={styles.bottomLine}></View>
+    <View style={styles.bottomLine}></View>
+    <Cell cell={square[6]} margin={['left']}/>
+    <View style={styles.sideLine}></View>
+    <Cell cell={square[7]}/>
+    <View style={styles.sideLine}></View>
+    <Cell cell={square[8]}/>
   </View>
 }
 
@@ -17,7 +38,6 @@ export default function Grid({level}) {
   var [board, updateBoard] = useState(sudoku.generate(level))
   return (
     <View style={styles.board}>
-      {/* {board.map((square,i) => <Square key={i} square={square}/>)} */}
       <Square square={board[0]} margin={['right', 'bottom']}/>
       <Square square={board[1]} margin={['right', 'bottom']}/>
       <Square square={board[2]} margin={['bottom']}/>
@@ -32,13 +52,14 @@ export default function Grid({level}) {
 }
 
 var marginWidth = 5;
+var colorTheme = 'pink';
 
 const styles = StyleSheet.create({
   board: {
     justifyContent: 'space-evenly',
     flexDirection: 'row',
     flexWrap: 'wrap',
-    backgroundColor: 'pink',
+    backgroundColor: colorTheme,
   },
   right: {
     marginRight: marginWidth
@@ -59,12 +80,27 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+  },
+  sideLine: {
+    backgroundColor: 'black',
+    width: 1,
+    height: 27
+  },
+  bottomLine: {
+    backgroundColor: 'black',
+    width: 30,
+    height: 1,
+    marginRight: 3,
+    marginLeft: 3
   },
   cell: {
-    height: 38,
-    width: 38,
+    height: 35,
+    width: 35,
     alignItems: 'center',
     justifyContent: 'center',
-  }
+  },
+  cellTop: {
+    marginTop: marginWidth + 5
+  },
 });
