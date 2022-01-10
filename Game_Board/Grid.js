e51f68dad7362c4e1a1d1e12b3a6be89435dfa48
Joseph Nahm <josephnahm1@gmail.com>
2022-01-09 18:57:06 -0800

    individual squares

--- Game_Board/Grid.js
+++ Game_Board/Grid.js
@@ -6,9 +6,10 @@ var Cell = ({cell}) => {
   return <View style={styles.cell}><Text>{cell}</Text></View>
 }
 
-var Square = ({square}) => {
-  return <View style={styles.square}>
-    {square.map((cell, i) => <Cell key={i} cell={cell}/>)}
+var Square = ({square, margin}) => {
+  var margins = margin ? margin.map(dir => styles[dir]) : [];
+  return <View style={[styles.square, ...margins]}>
+    {/* {square.map((cell, i) => <Cell key={i} cell={cell}/>)} */}
   </View>
 }
 
@@ -16,35 +17,57 @@ export default function Grid({level}) {
   var [board, updateBoard] = useState(sudoku.generate(level))
   return (
     <View style={styles.board}>
-      {board.map((square,i) => <Square key={i} square={square}/>)}
+      {/* {board.map((square,i) => <Square key={i} square={square}/>)} */}
+      <Square square={board[0]} margin={['right', 'bottom']}/>
+      <Square square={board[1]} margin={['right', 'bottom', 'left']}/>
+      <Square square={board[2]} margin={['left', 'bottom']}/>
+      <Square square={board[3]} margin={['top', 'right', 'bottom']}/>
+      <Square square={board[4]} margin={['top', 'right', 'bottom', 'left']}/>
+      <Square square={board[5]} margin={['top', 'left', 'bottom']}/>
+      <Square square={board[6]} margin={['right', 'bottom']}/>
+      <Square square={board[7]} margin={['right', 'bottom']}/>
+      <Square square={board[8]} margin={['right', 'bottom']}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   board: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-    flexWrap: 'wrap'
-  },
-  square: {
-    borderColor: 'black',
-    borderWidth: 0.5,
-    height: 120,
-    width: 120,
+    justifyContent: 'space-evenly',
     flexDirection: 'row',
     flexWrap: 'wrap',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'pink',
+  },
+  right: {
+    marginRight: 4
+  },
+  left: {
+    marginLeft: 4
+  },
+  bottom: {
+    marginBottom: 4
+  },
+  top: {
+    marginTop: 4
+  },
+  square: {
+    height: 120,
+    width: 120,
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderWidth: 2
+
+    // flexDirection: 'row',
+    // flexWrap: 'wrap',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
   cell: {
-    borderColor: 'black',
-    borderWidth: 0.5,
-    height: 38,
-    width: 38,
-    alignItems: 'center',
-    justifyContent: 'center',
+    // borderColor: 'black',
+    // borderWidth: 0.5,
+    // height: 38,
+    // width: 38,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   }
 });
