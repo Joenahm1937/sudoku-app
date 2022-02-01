76a5185236d6c03e484b9eae2b06d3e637f59c5c
Joseph Nahm <josephnahm1@gmail.com>
2022-01-31 22:16:22 -0800

    fixed weird game start render bug:

--- Game_Board/Lives.js
+++ Game_Board/Lives.js
@@ -1,32 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
-import { useEffect } from 'react';
-import AppLoading from 'expo-app-loading';
-import { useFonts, Montserrat_400Regular } from '@expo-google-fonts/montserrat';
+import { StyleSheet, Text, View } from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { useEffect } from "react";
+import AppLoading from "expo-app-loading";
+import { useFonts, Montserrat_400Regular } from "@expo-google-fonts/montserrat";
 
-export default function Pieces({lives}) {
+export default function Pieces({ lives }) {
   let [fontsLoaded] = useFonts({
     Montserrat_400Regular,
   });
 
   if (!fontsLoaded) {
-    return <AppLoading />;
+    // return <AppLoading />;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.size}>
+          Lives: {lives > 0 ? [...Array(lives)].fill("I").join("") : 0}
+        </Text>
+      </View>
+    );
   } else {
-    return <View style={styles.container}>
-      <Text style={styles.life}>Lives: {lives > 0 ? [...Array(lives)].fill('I').join('') : 0}</Text>
-    </View>
+    return (
+      <View style={styles.container}>
+        <Text style={[styles.size, styles.family]}>
+          Lives: {lives > 0 ? [...Array(lives)].fill("I").join("") : 0}
+        </Text>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: hp('8%'),
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    height: hp("8%"),
   },
-  life: {
+  size: {
     fontSize: 20,
-    fontFamily: 'Montserrat_400Regular'
-  }
+  },
+  family: {
+    fontFamily: "Montserrat_400Regular",
+  },
 });
